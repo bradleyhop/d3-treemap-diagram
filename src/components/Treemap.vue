fdee7655433ae25183983c30a4919438ae49031f
bradleyhop <bradleyhop@gmail.com>
2021-03-13 17:57:38 -0600

    treemap rect and text draws; passes 6/12 tests

--- src/components/Treemap.vue
+++ src/components/Treemap.vue
@@ -8,15 +8,24 @@ export default {
     return {
       loading: true, // conditional redning variable to show basic loading status to user
       // placeholder for the data set
+      dataUrl:
+      'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json',
       gameData: undefined,
-      mapViewBox: '0, 0, 1200, 610', // position of BOTH state and county maps within the svg
-      mapPosition: '120, 0', // offset for map position
+      widthViewPort: 1200,
+      heightViewPort: 800,
+      mapPosition: '0, 0', // offset for map position
     };
   },
 
   created() {
-    console.log('hello');
-    this.loading = false;
+    fetch(this.dataUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // store obj within vue; make non-reactive since this data isn't going to change; speedup
+        this.gameData = Object.freeze(data);
+      })
+      .then(() => this.graphInit())
+      .catch((error) => console.log(error));
   },
 
   methods: {
@@ -24,15 +33,48 @@ export default {
       // remove loading message
       this.loading = false;
 
-      const svg = d3.select('#choropleth')
+      const svg = d3.select('#treemap')
         .append('svg')
-        .attr('viewBox', this.mapViewBox);
+        .attr('width', this.widthViewPort)
+        .attr('height', this.heightViewPort);
 
       // svg group for the mapping of data; helps keep data graphics separate from axis
       const map = svg.append('g')
         .attr('id', 'map')
         .attr('class', 'map')
         .attr('transform', `translate(${this.mapPosition})`);
+
+      const root = d3.hierarchy(this.gameData).sum((d) => d.value);
+
+      d3.treemap().size([this.widthViewPort, this.heightViewPort]).padding(1)(root);
+
+      // draw the boxes
+      map.selectAll('rect')
+        .data(root.leaves())
+        .enter()
+        .append('rect')
+        .attr('class', 'tile') // project requirement
+        .attr('x', (d) => d.x0)
+        .attr('y', (d) => d.y0)
+        .attr('width', (d) => d.x1 - d.x0)
+        .attr('height', (d) => d.y1 - d.y0)
+        // next three attributes are project requirements
+        .attr('data-name', (d) => d.data.name)
+        .attr('data-category', (d) => d.data.category)
+        .attr('data-value', (d) => d.data.value)
+        .style('stroke', 'black')
+        .style('fill', 'green');
+
+      // add text within boxes
+      map.selectAll('text')
+        .data(root.leaves())
+        .enter()
+        .append('text')
+        .attr('x', (d) => d.x0 + 5)
+        .attr('y', (d) => d.y0 + 12)
+        .text((d) => d.data.name)
+        .attr('font-size', '10px')
+        .attr('fill', 'white');
 
       // function declaration for tooltip div element
       const divTool = d3.select('#tooltip-container')
@@ -41,7 +83,6 @@ export default {
         .style('visibility', 'hidden');
 
       // placeholders to quiet eslint
-      map.append('g');
       divTool.append('g');
     },
   },
@@ -49,7 +90,7 @@ export default {
 </script>
 
 <template>
-  <div class="conainter-choropleth">
+  <div class="conainter-treemap">
     <!-- tooltip info displays on mouseover here; id is project requirement -->
     <div
       id="tooltip-container"
@@ -63,10 +104,10 @@ export default {
       Loading...
       </p>
     </div>
-    <!-- d3 choropleth map is drawn at #choropleth -->
+    <!-- d3 treemap map is drawn at #treemap -->
     <div
-      class="choropleth"
-      id="choropleth"
+      class="treemap"
+      id="treemap"
       >
     </div>
   </div>
@@ -75,30 +116,14 @@ export default {
 <style lang="scss">
 // DO NOT SCOPE THIS COMPONENT'S STYLE: D3 won't be able to see it
 
-// set container width so that map scales with the viewport; in conjunction with svg attribute
-//  viewBox
-.choropleth {
+.treemap {
   margin: auto;
-  width: 65%;
+  width: 85%;
 
   // scale for tablets and phones
   @media only screen and  (max-width: 800px) {
     width: 100%;
   }
-}
-
-.county:hover {
-  fill: #fff;
-  stroke: #616161;
-}
-
-.state {
-  stroke: $app-background;
-}
-
-// legend tick
-.tick text {
-  font-size: 0.9rem;
 }
 
 .tooltip-container {
